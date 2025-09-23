fcf771f542053b5fad787188974020c3a2776260
Arkady Schoenberg <shasoka@yandex.ru>
2025-09-23 14:02:26 +0700

    Set up db config and db dependency

--- app/core/config.py
+++ app/core/config.py
@@ -38,20 +38,20 @@ class ApiBaseConfig(BaseModel):
     ...
 
 
-# class Database(BaseModel):
-# 	url: PostgresDsn  # подтягивается из .env
-# 	echo: bool = False
-# 	echo_pool: bool = False
-# 	max_overflow: int = 50
-# 	pool_size: int = 10
-#
-# 	naming_convention: dict[str, str] = {
-# 		"ix": "ix_%(column_0_label)s",
-# 		"uq": "uq_%(table_name)s_%(column_0_N_name)s",
-# 		"ck": "ck_%(table_name)s_%(constraint_name)s",
-# 		"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-# 		"pk": "pk_%(table_name)s",
-# 	}
+class Database(BaseModel):
+    url: PostgresDsn  # подтягивается из .env
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 50
+    pool_size: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class Settings(BaseSettings):
@@ -66,7 +66,7 @@ class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     app: AppObjectConfig = AppObjectConfig()
     api: ApiBaseConfig = ApiBaseConfig()
-    # db: Database
+    db: Database
 
 
 # noinspection PyArgumentList
---
+++ app/models/db_setup.py
@@ -0,0 +1,52 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from typing import AsyncGenerator
+
+from core.config import settings
+
+
+class DatabaseSetup:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,  # Вывод информации о запросах
+        echo_pool: bool = False,  # Вывод информации о подключениях
+        max_overflow: int = 10,  # Количество переполнения подключений
+        pool_size: int = 5,  # Количество одновременных подключений
+    ) -> None:
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            max_overflow=max_overflow,
+            pool_size=pool_size,
+        )
+
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+# Объект, используемый в механизме внедрения зависимостей для получения
+# подключения к БД
+db_helper = DatabaseSetup(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    max_overflow=settings.db.max_overflow,
+    pool_size=settings.db.pool_size,
+)

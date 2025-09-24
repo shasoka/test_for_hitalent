96f7046de4897b3bd65847ae92bb351144c891b8
Arkady Schoenberg <shasoka@yandex.ru>
2025-09-25 03:34:07 +0700

    Implemented test case for successful question deletion with its' answers

--- tests/integration/test_questions_api.py
+++ tests/integration/test_questions_api.py
@@ -14,15 +14,85 @@ from app.core.models import Answer, Question
 class TestQuestionsAPI:
 
     @staticmethod
+    async def _take_answers_snaphost(
+        db_session: AsyncSession,
+    ) -> list[Answer]:
+        query_result: QueryResult = await db_session.execute(select(Answer))
+        return list(query_result.scalars().all())
+
+    @staticmethod
+    async def _take_questions_snaphost(
+        db_session: AsyncSession,
+    ) -> list[Question]:
+        query_result: QueryResult = await db_session.execute(select(Question))
+        return list(query_result.scalars().all())
+
+    @staticmethod
+    async def _get_question_selectinload(
+        question_id: int,
+        db_session: AsyncSession,
+    ) -> Question | None:
+        query_result: QueryResult = await db_session.execute(
+            select(Question)
+            .where(Question.id == question_id)
+            .options(selectinload(Question.answers))
+        )
+        return query_result.scalar_one_or_none()
+
+    @staticmethod
+    async def _get_question(
+        question_id: int,
+        db_session: AsyncSession,
+    ) -> Question | None:
+        query_result: QueryResult = await db_session.execute(
+            select(Question).where(Question.id == question_id)
+        )
+        return query_result.scalar_one_or_none()
+
+    @staticmethod
+    async def _get_answer(
+        answer_id: int,
+        db_session: AsyncSession,
+    ) -> Answer | None:
+        query_result: QueryResult = await db_session.execute(
+            select(Answer).where(Answer.id == answer_id)
+        )
+        return query_result.scalar_one_or_none()
+
+    @staticmethod
+    async def _add_question_and_answer(
+        db_session: AsyncSession,
+        question_text: str,
+        answer_text: str,
+    ) -> tuple[Question, Answer, uuid.UUID]:
+        question: Question = Question(text=question_text)
+        db_session.add(question)
+        await db_session.commit()
+        await db_session.refresh(question)
+
+        user_id: uuid.UUID = uuid.uuid4()
+        answer: Answer = Answer(
+            user_id=user_id,
+            text=answer_text,
+            question_id=question.id,
+        )
+        db_session.add(answer)
+
+        await db_session.commit()
+        await db_session.refresh(question)
+
+        return question, answer, user_id
+
     async def _post_question(
+        self,
         db_session: AsyncSession,
         base_url: str,
         questions_prefix: str,
         text: str,
     ) -> tuple[list[Question], list[Question], dict, int]:
-        # Сохранение количества вопросов до добавления нового
-        old_query_result: QueryResult = await db_session.execute(select(Question))
-        old_db_questions: list[Question] = list(old_query_result.scalars().all())
+        old_db_questions: list[Question] = await self._take_questions_snaphost(
+            db_session=db_session,
+        )
 
         async with AsyncClient(
             transport=ASGITransport(app=app),
@@ -36,8 +106,9 @@ class TestQuestionsAPI:
             )
 
         response_data: dict = response.json()
-        new_query_result: QueryResult = await db_session.execute(select(Question))
-        new_db_questions: list[Question] = list(new_query_result.scalars().all())
+        new_db_questions: list[Question] = await self._take_questions_snaphost(
+            db_session=db_session,
+        )
 
         return old_db_questions, new_db_questions, response_data, response.status_code
 
@@ -83,8 +154,9 @@ class TestQuestionsAPI:
 
         # Проверка
         response_data: dict = response.json()
-        query_result: QueryResult = await db_session.execute(select(Question))
-        db_questions: list[Question] = list(query_result.scalars().all())
+        db_questions: list[Question] = await self._take_questions_snaphost(
+            db_session=db_session,
+        )
 
         assert response.status_code == 200
         assert response_data[0]["text"] == "Question to get"
@@ -146,33 +218,19 @@ class TestQuestionsAPI:
             base_url=base_url,
         ) as ac:
             # Добавление вопроса и ответа в БД
-            question: Question = Question(text="Question to get")
-            db_session.add(question)
-            await db_session.commit()
-            await db_session.refresh(question)
-
-            user_id: uuid.UUID = uuid.uuid4()
-            answer: Answer = Answer(
-                user_id=user_id,
-                text="Answer to get",
-                question_id=question.id,
+            question, answer, user_id = await self._add_question_and_answer(
+                db_session=db_session,
+                question_text="Question to get",
+                answer_text="Answer to get",
             )
-            db_session.add(answer)
 
-            await db_session.commit()
-            await db_session.refresh(question)
-
-            # Выполнение запроса
             response: Response = await ac.get(questions_prefix + f"{question.id}")
 
-        # Проверка
         response_data: dict = response.json()
-        query_result: QueryResult = await db_session.execute(
-            select(Question)
-            .where(Question.id == question.id)
-            .options(selectinload(Question.answers))
+        db_question: Question | None = await self._get_question_selectinload(
+            db_session=db_session,
+            question_id=question.id,
         )
-        db_question: Question | None = query_result.scalar_one_or_none()
 
         assert response.status_code == 200
         assert db_question is not None
@@ -199,18 +257,70 @@ class TestQuestionsAPI:
             transport=ASGITransport(app=app),
             base_url=base_url,
         ) as ac:
-            # Выполнение запроса
             response: Response = await ac.get(questions_prefix + "0")
 
-        # Проверка
         response_data: dict = response.json()
-        query_result: QueryResult = await db_session.execute(
-            select(Question)
-            .where(Question.id == 0)
-            .options(selectinload(Question.answers))
+        db_question: Question | None = await self._get_question_selectinload(
+            db_session=db_session,
+            question_id=0,
         )
-        db_question: Question | None = query_result.scalar_one_or_none()
 
         assert response.status_code == 404
         assert db_question is None
-        assert response_data["message"] == "Сущность не найдена"
+        assert response_data["message"] == "Сущность не найдена"
+
+    async def test_delete_question_with_answers_by_id_200(
+        self,
+        db_session: AsyncSession,
+        base_url: str,
+        questions_prefix: str,
+    ):
+        async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url=base_url,
+        ) as ac:
+            # Добавление вопроса и ответа в БД
+            question, answer, user_id = await self._add_question_and_answer(
+                db_session=db_session,
+                question_text="Question to delete",
+                answer_text="Answer to delete",
+            )
+
+            # Сохранение количества вопросов и ответов до удаления
+            old_db_questions: list[Question] = await self._take_questions_snaphost(
+                db_session=db_session
+            )
+            old_db_answers: list[Answer] = await self._take_answers_snaphost(
+                db_session=db_session
+            )
+
+            response: Response = await ac.delete(questions_prefix + f"{question.id}")
+
+        response_data: dict = response.json()
+
+        # Получение новых списков вопросов и ответов
+        new_db_questions: list[Question] = await self._take_questions_snaphost(
+            db_session=db_session
+        )
+        new_db_answers: list[Answer] = await self._take_answers_snaphost(
+            db_session=db_session
+        )
+
+        # Получение вопроса после удаления
+        deleted_db_question: Question | None = await self._get_question(
+            db_session=db_session,
+            question_id=question.id,
+        )
+
+        # Получение ответа после удаления
+        deleted_db_answer: Answer | None = await self._get_answer(
+            db_session=db_session,
+            answer_id=answer.id,
+        )
+
+        assert response.status_code == 200
+        assert response_data["text"] == question.text == "Question to delete"
+        assert deleted_db_question is None
+        assert deleted_db_answer is None
+        assert len(new_db_questions) + 1 == len(old_db_questions)
+        assert len(new_db_answers) + 1 == len(old_db_answers)
